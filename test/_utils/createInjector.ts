b2ed3f02b00e230459114a3ada4d6c5ffbddfd22
Cole <cqiufan@outlook.com>
2022-05-04 10:36:54 +0800

    test: add test for frameworkFactory

--- test/_utils/createInjector.ts
+++ test/_utils/createInjector.ts
@@ -3,7 +3,7 @@ import type { ConfigOptions } from 'karma';
 import type { DiFactory } from '@/types/diFactory';
 import viteServerFactory from '@/factory/viteServerFactory';
 import { scheduleMock, serveFileMock } from './mockFn';
-import { merge, noop } from 'lodash';
+import { mergeWith, noop } from 'lodash';
 
 const executor: DiFactory = () => ({
   schedule: scheduleMock,
@@ -24,16 +24,29 @@ logger.$inject = [];
 const serveFile: DiFactory = () => serveFileMock;
 serveFile.$inject = [];
 
-const commonConfig = {
-  basePath: './',
-  urlRoot: '/',
-  reporters: [],
-};
+class Config {
+  basePath = './';
+  urlRoot = '/';
+  reporters = [];
+  frameworks = [];
+  plugins = [];
+  files = [];
 
-export default function createInjector(config?: ConfigOptions) {
+  set(newConfig: ConfigOptions) {
+    mergeWith(this, newConfig, (obj, src) => {
+      if (Array.isArray(src)) {
+        return src;
+      }
+    });
+  }
+}
+
+export default function createInjector(userConfig?: ConfigOptions) {
+  const config = new Config();
+  userConfig && config.set(userConfig);
   const injector = new Injector([
     {
-      config: ['value', merge({}, commonConfig, config)],
+      config: ['value', config],
       logger: ['factory', logger],
       serveFile: ['factory', serveFile],
       vite: ['factory', viteServerFactory],
---
+++ test/frameworkFactory.spec.ts
@@ -0,0 +1,43 @@
+import frameworkFactory from '@/factory/frameworkFactory';
+import createInjector from './_utils/createInjector';
+
+describe('frameworkFactory', () => {
+  it('should return viteServer', async () => {
+    const injector = createInjector();
+    const viteServer = await injector.get('vite');
+    const framework = await injector.invoke(frameworkFactory);
+    expect(framework).toBe(viteServer);
+  });
+
+  it('should auto init correctly in default config', async () => {
+    const injector = createInjector();
+    await injector.invoke(frameworkFactory);
+    const config = injector.get('config');
+    expect(config).toMatchSnapshot();
+  });
+
+  it('should not auto init if vite.autoInit is false in config', async () => {
+    const injector = createInjector({
+      vite: {
+        autoInit: false,
+      },
+    });
+    await injector.invoke(frameworkFactory);
+    const config = injector.get('config');
+    expect(config).toMatchSnapshot();
+  });
+
+  it('rewrite the files property of config correctly', async () => {
+    const injector = createInjector({
+      files: [
+        'a.ts',
+        { pattern: 'b.ts', included: false, vite: true },
+        { pattern: 'b.ts', served: true, vite: true },
+        { pattern: 'c.ts', vite: true },
+      ],
+    });
+    await injector.invoke(frameworkFactory);
+    const config = injector.get('config');
+    expect(config).toMatchSnapshot();
+  });
+});
